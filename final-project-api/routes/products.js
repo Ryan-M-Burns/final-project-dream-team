269793545744ed08ac857d4d0062dbfcc781cb0a
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-11-25 11:21:54 +0000

    fix

--- final-project-api/routes/products.js
+++ final-project-api/routes/products.js
@@ -2,27 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('farm profile goes here');
+  res.send('products go here');
 });
 
 router.get('/:id', (req, res) => {
-  res.send('farm profile goes here');
+  res.send('products go here');
 });
 
 router.get('/new', (req, res) => {
-  res.send('new farm form goes here');
+  res.send('The farmers new product goes here');
 });
 
 router.get('/:id/edit', (req, res) => {
-  res.send('farm profile EDIT form goes here');
+  res.send('Product edit goes here');
 });
 
 router.put('/:id', (req, res) => {
-  res.send('farm profile UPDATE goes here');
+  res.send('Updated products go here');
 });
 
 router.delete('/:id', (req, res) => {
-  res.send('farm profile DELETE goes here');
+  res.send('deleted products go here');
 });
+
+
 
 module.exports = router;
