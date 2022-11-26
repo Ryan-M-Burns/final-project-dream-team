6f9d86e82d16165f4e8f36abc334ce9fbd7a62c2
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-26 13:41:03 -0800

    create templates and import into App

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,28 @@ import logo from './logo.svg';
 import axios from 'axios';
 import './App.scss';
 
+import About from './About';
+import ContactUs from './ContactUs';
+import Farm from './Farm';
+import Home from './Home';
+import Landing from './Landing';
+import Navbar from './Navbar';
+import RegisterFarm from './RegisterFarm';
+import RegisterUser from './RegisterUser';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
