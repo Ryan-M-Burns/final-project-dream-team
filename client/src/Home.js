b0b7f12660130c26a53ec48f2ce633a5b9cb61fd
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-27 11:53:45 -0800

    add buttons with links, create carousel file

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import NavButton from './components/home-page/NavButton';
 import './Home.scss';
 const Home = () => {
   return (
-    <div className='home'>🏠 is where the 💖 is</div>
+    <section>
+      <BoxCarousel />
+      <ul>
+        <li>
+          <NavButton link={"/produce"} name={"PRODUCE"} />
+          <NavButton link={"/boxes"} name={"MADE FOR YOU"} />
+          <NavButton link={"/meet-the-farmers"} name={"MEET OUR FARMERS"} />
+        </li>
+        <li>
+          <NavButton link={"/our-mission"} name={"OUR MISSION"} />
+          <NavButton link={"/produce"} name={"PRODUCE"} />
+          <NavButton link={"/contact"} name={"REACH OUT"} />
+        </li>
+      </ul>
+    </section>
+
   );
 };
 
