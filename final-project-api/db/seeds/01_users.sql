fd3e04df7b34ac283e3e08e22982bb44ed2487ba
Taylor Smith <taylor.l.smith07@gmail.com>
2022-11-26 16:25:13 -0800

    add orders seeds

--- final-project-api/db/seeds/01_users.sql
+++ final-project-api/db/seeds/01_users.sql
@@ -2,5 +2,6 @@ INSERT INTO users (name, email, password, is_farmer, street_address, city, posta
 VALUES
 ('Taylor Smith', 'tay@harvest.com', '1234', true, '123 Fake Street', 'Victoria', 'V9A-6W3', '250-857-3617','2022-09-25T07:00:00.000Z'),
 ('Ryan Burns', 'ryan@harvest.com', '1234', true, '123 Fake Street', 'Victoria', 'V9A-6W3', '250-857-3617','2022-09-25T07:00:00.000Z'),
-('Anouk Van Bogaert', 'anouk@harvest.com', '1234', true, '123 Fake Street', 'Victoria', 'V9A-6W3', '250-857-3617','2022-09-25T07:00:00.000Z')
+('Anouk Van Bogaert', 'anouk@harvest.com', '1234', true, '123 Fake Street', 'Victoria', 'V9A-6W3', '250-857-3617','2022-09-25T07:00:00.000Z'),
+('Evan Fowler', 'customer@harvest.com', '1234', false, '123 Fake Street', 'Victoria', 'V9A-6W3', '250-857-3617','2022-09-25T07:00:00.000Z')
 ;
