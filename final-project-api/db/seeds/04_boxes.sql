537ee262694fe04c9cc2dd2fc763f31f85f6fd8d
Taylor Smith <taylor.l.smith07@gmail.com>
2022-11-26 16:03:23 -0800

    add seeds for boxes table

---
+++ final-project-api/db/seeds/04_boxes.sql
@@ -0,0 +1,13 @@
+INSERT INTO boxes (name, type, price, quantity, active_status)
+VALUES
+('Root Vegetable Mix', 'pre-built', 30, 20, true),
+('Fall Mix', 'pre-built', 30, 20, true),
+('Family Bundle Box', 'pre-built', 50, 20, true),
+('Herbs Box', 'pre-built', 15, 10, true),
+('Assorted Fruit Box', 'pre-built', 30, 10, true),
+('Harvest Taster Box', 'pre-built', 35, 10, true),
+('Salsa Lovers Box', 'pre-built', 35, 10, true)
+;
+
+
+
