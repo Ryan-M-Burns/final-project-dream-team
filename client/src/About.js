277c75760171932a5790e1d52a67a13fefa157ed
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-26 21:36:10 -0800

    add about page

--- client/src/About.js
+++ client/src/About.js
@@ -3,7 +3,21 @@ import './About.scss';
 
 const About = () => {
   return (
-    <div>Dream Team</div>
+    <section>
+      <h2>Our Mission</h2>
+      <p>we believe in connecting farmers and their
+        communities. We want to provide families with the
+        freshest produce straight from the soil with the
+        convenience that our society has come to expect.
+        We hope that you feel as passionate as we do about sourcing
+        local and we look forward to developing a long lasting
+        relationship with you!
+
+        Sincerely,
+
+        The team at Harvest
+      </p>
+    </section>
   );
 };
 
