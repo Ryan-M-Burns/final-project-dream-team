bf35eb9e89bd26917e34c05793ae3e2eb2166f69
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-26 17:30:12 -0800

    create react skeleton

---
+++ client/src/consumers/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from './Login.scss';
+const Login = () => {
+  return (
+    <div>Login</div>
+  );
+};
+
+export default Login;
---
+++ client/src/farmers/FarmerLogin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FarmerLogin from 'FarmerLogin.scss';
+
+const FarmerLogin = () => {
+  return (
+    <div>FarmerLogin</div>
+  );
+};
+
+export default FarmerLogin;
