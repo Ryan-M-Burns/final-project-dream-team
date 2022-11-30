c4810cae6ab9d16533402ebd71c5390ef3550e8d
Taylor Smith <taylor.l.smith07@gmail.com>
2022-11-30 13:27:35 -0800

    add test for query

--- final-project-api/db/queries/categories.js
+++ final-project-api/db/queries/categories.js
@@ -5,6 +5,7 @@ export const getCategories = () => {
   return db
   .query(`SELECT DISTINCT category FROM products;`)
   .then(categories => {
+    console.log('categories', categories.rows)
     return categories.rows;
   })
   .catch(err => {
