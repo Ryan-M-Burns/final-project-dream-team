3ef19e78c67f30539799f563811f7d0f594b39bf
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2022-11-30 12:32:10 -0800

    css

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -7,20 +7,26 @@ const Landing = () => {
       {/* page 1 */}
       <div className="landing__page">
         <div className="title__andbuttons">
-          <h1 className="title"> HARVEST </h1>
+          <h1 id="title"> HARVEST </h1>
           <button>Visit our page</button>
         </div>
-        <div>
-          <img></img>
+        <div classnName="arrow">
+          hello
         </div>
       </div>
 
       {/* page 2 */}
       <div className="row__login">
-        <div className="user__login">
+        <div className="login">
+          <form>
+            <label>username</label>
+            <input type="text"></input>
+            <label>email</label>
+            <input type="text"></input>
+          </form>
 
         </div>
-        <div className="farmer__login">
+        <div className="login">
 
         </div>
       </div>
