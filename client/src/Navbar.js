8ac3f31feb2d723d00b3fd547990ae1b22640917
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2022-11-29 19:42:07 -0800

    bones added - no css

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './Navbar.scss';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 // Drop down menu for account
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleDropdown = () => {
+    setOpen(!open);
+  };
+
   return (
     <nav>
       <div>
         <Link to='/'>
-          <button>Back to Home</button>
-        </Link>
-        <Link to='/account'> 
-          <button>Account</button>
+          <h1>HARVEST</h1>
         </Link>
       </div>
-      <h1> HARVEST </h1>
+      <div className="dropdown">
+        <button onClick={handleDropdown}>Account</button>
+        {open ? (
+          <ul className="account">
+            <li className="account-item">
+              <button>View your profile</button>
+            </li>
+            <li className="account-item">
+              <button>My orders</button>
+            </li>
+            <li className="account-item">
+              <button>Sign out</button>
+            </li>
+          </ul>
+        ) : null}
+      </div>
       <div>
         <Link to='/our-mission'>
           <button>About Us</button>
