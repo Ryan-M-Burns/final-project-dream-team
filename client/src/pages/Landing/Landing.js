0c91573d2cac9096543d85182344b2592fe6f336
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2022-11-30 13:16:52 -0800

    landing page layout

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -8,28 +8,32 @@ const Landing = () => {
       <div className="landing__page">
         <div className="title__andbuttons">
           <h1 id="title"> HARVEST </h1>
-          <button>Visit our page</button>
+          <button id="visit">Visit our page</button>
+          <div classnName="arrow">
+            hello
+          </div>
         </div>
-        <div classnName="arrow">
-          hello
+        {/* page 2 */}
+        <div className="row__login">
+          <div className="login">
+            <form>
+              <label>username</label>
+              <input type="text"></input>
+              <label>email</label>
+              <input type="text"></input>
+            </form>
+          </div>
+          <div className="login">
+            <form>
+              <label>username</label>
+              <input type="text"></input>
+              <label>email</label>
+              <input type="text"></input>
+            </form>
+          </div>
         </div>
       </div>
 
-      {/* page 2 */}
-      <div className="row__login">
-        <div className="login">
-          <form>
-            <label>username</label>
-            <input type="text"></input>
-            <label>email</label>
-            <input type="text"></input>
-          </form>
-
-        </div>
-        <div className="login">
-
-        </div>
-      </div>
 
     </div>
   );
