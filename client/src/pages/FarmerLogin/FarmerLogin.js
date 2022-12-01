076c1392b01d0b692920d16fad44825a6d6ce05b
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2022-12-01 08:25:31 -0800

    basic login

--- client/src/pages/FarmerLogin/FarmerLogin.js
+++ client/src/pages/FarmerLogin/FarmerLogin.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import {unstable_isMuiElement} from '@mui/utils';
+import axios from 'axios';
+import {useState, useEffect, React} from 'react';
 import './FarmerLogin.scss';
+import {Link} from 'react-router-dom';
 
 
 const FarmerLogin = () => {
+  const [user, setUser] = useState("");
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const getUsers = () => {
+    axios.get('/users');
+  };
+
+  // useEffect(() => {
+  //   getUser();
+  // }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const users = await getUsers();
+
+    const userInfo = users.find(user => user.name === username);
+
+    if (userInfo && user.password === password) {
+      setUser(userInfo);
+      window.location.href = "about";
+    }
+  };
+
   return (
 
     <div className="row__login">
@@ -14,14 +42,15 @@ const FarmerLogin = () => {
         </div>
         <form className="farmer__login">
           <label className="farmer__label">username</label>
-          <input className="farmer__input" type="text"></input>
+          <input className="farmer__input" type="text" onChange={e => setUserName(e.target.value)}></input>
           <label className="farmer__label">password</label>
-          <input className="farmer__input" type="text"></input>
-        </form>
-      </div>
-      <div className="login__button">
-        <button>
-          <p>Login</p></button>
+          <input className="farmer__input" type="password" onChange={e => setPassword(e.target.value)}></input>
+          <div>
+            <Link to='/home'>
+              <input className="submit__button" type="submit" value="Submit" onSubmit={handleSubmit}></input>
+            </Link>
+          </div>
+        </form >
       </div>
     </div>
   );
