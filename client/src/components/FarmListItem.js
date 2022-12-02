23eb071af0c0117ccb13c819df879c35b9077d2e
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-12-01 16:02:43 -0800

    remove testing console.log

--- client/src/components/FarmListItem.js
+++ client/src/components/FarmListItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './FarmListItem.scss'
-const FarmListItem = ({farm}) => {
-
+import './FarmListItem.scss';
+const FarmListItem = ({ farm }) => {
   return (
     <li>
       <img src={farm.logo_url} alt="farm_logo" className="farm-logo" />
