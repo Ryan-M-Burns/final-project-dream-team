526b50e5abb5a31ff344dbc4dfe912d91a7eb6f5
Taylor Smith <taylor.l.smith07@gmail.com>
2022-12-01 21:03:48 -0800

    add logic to create product component

--- client/src/farmers/CreateProduct.js
+++ client/src/farmers/CreateProduct.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const CreateProduct = () => {
-  return (
-    <div>CreateProduct</div>
-  )
+  function handleSubmit (formData) {
+    axios.post('/products', formData).then(() => alert('you did it!'))
+  }
+  return <div>
+    <h1>Hi! Let's make a product</h1>
+    <ProductForm onSubmit={handleSubmit} />
+   </div>
 }
 
 export default CreateProduct
