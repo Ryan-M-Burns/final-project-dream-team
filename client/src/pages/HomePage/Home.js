5ed097dc8753797c57172c295e3749956d400e26
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2022-12-02 14:34:09 -0800

    fixed

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -1,5 +1,5 @@
-import { React, useEffect } from 'react';
-import { getFilteredProducts, getFarms } from '../../helpers/selectors';
+import {React, useEffect} from 'react';
+import {getFilteredProducts, getFarms} from '../../helpers/selectors';
 import FarmList from '../../components/FarmList';
 import ProductList from '../../components/ProductList';
 import CategoryList from './HomePageComponents/CategoryList';
