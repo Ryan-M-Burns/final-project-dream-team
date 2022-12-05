117c8d11d2f6e95103dedddb6b05e3cd0cf5aefd
Anouk Van Bogaert <anoukvanbogaert@Anouks-MBP.home>
2022-12-04 18:57:01 -0800

    num of items in cart now show in nav

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = (
     showCart,
     setCartDrawer,
     setUserlogin,
-    userlogin
+    userlogin, cart
   }) => {
 
   console.log('showCart', showCart);
@@ -97,7 +97,7 @@ const Navbar = (
           </Link>
         </div>
         <div>
-          <button className="nav__cart" onClick={() => setCartDrawer(!showCart)}><img src="../images/cart.png" alt="cart"></img></button>
+          <button className="nav__cart" onClick={() => setCartDrawer(!showCart)}><img src="../images/cart.png" alt="cart"></img>({cart.length})</button>
         </div>
       </div>
     </nav>
